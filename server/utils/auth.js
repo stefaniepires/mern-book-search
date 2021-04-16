8a6fc722c31247b964750d43858f9b135803f122
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-04-15 22:25:59 -0500

    added schemas

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'secret';
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -25,11 +25,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+     
     }
 
     // send to next endpoint
-    next();
+  
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
